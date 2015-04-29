5720b930025d219d67085a6cca1aa2cbd5fd21fa
Courtney <courtney.mae.phillips@gmail.com>
2015-04-29 15:43:22 -0700

    Remove prior logic and specs in order to persue different approach to logic.

--- spec/interactive_dictionary_spec.rb
+++ spec/interactive_dictionary_spec.rb
@@ -4,21 +4,4 @@ require('word')
 
 describe(Definition) do
 
-  # describe('#retrieve_definition') do
-  #   it("retrieves the definition of a particular term") do
-  #     test_term=Word.new("spider")
-  #
-  #
-
-  # 
-  # describe('#define') do
-  #   it("attaches a definition to a term") do
-  #     test_term=Word.new("spider")
-  #     test_term.define("the ickiest, spookiest thing")
-  #     expect(test_term.define()).to(eq("spider"))
-  #   end
-  # end
-  #
-
-
 end
