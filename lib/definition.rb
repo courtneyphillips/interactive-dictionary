2badc915393162da3fc04381635c78c9a85f15a7
Courtney <courtney.mae.phillips@gmail.com>
2015-04-29 13:32:21 -0700

    Create classes for word and definition.

---
+++ lib/definition.rb
@@ -0,0 +1,6 @@
+class Definition
+  define_method(:initialize) do |definition|
+    @definition = definition
+  end
+
+end
---
+++ lib/word.rb
@@ -0,0 +1,5 @@
+class Word
+  define_method(:initialize) do |word|
+    @word = word
+  end
+end
