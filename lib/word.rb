065fe7e22c823701a9d5a5f9b1e55096ea062ffa
Courtney <courtney.mae.phillips@gmail.com>
2015-04-29 14:09:34 -0700

    Take user input terms. Test pass.

--- lib/word.rb
+++ lib/word.rb
@@ -1,5 +1,24 @@
 class Word
-  define_method(:initialize) do |word|
-    @word = word
+  @@dictionary = []
+
+  define_method(:initialize) do |term|
+    @term = term
   end
+
+  define_method(:term) do
+    @term
+  end
+
+  define_method(:save) do
+    @@dictionary.push(self)
+  end
+
+  define_singleton_method(:all) do
+    @@dictionary
+  end
+
+  define_singleton_method(:clear) do
+    @@dictionary = []
+  end
+
 end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -4,11 +4,10 @@ require('word')
 
 describe(Word) do
 
-  describe('#define') do
-    it("returns the definition of a given word") do
-      test_word = "humblebrag"
-      test_definition = "A modest statement that actually draws attention to something of which one is proud."
-      expect("humblebrag".define()).to(eq("A modest statement that actually draws attention to something of which one is proud."))
+  describe("#term") do
+    it("allows you to add a new term to the dictionary") do
+      test_term = Word.new("spider")
+      expect(test_term.term()).to(eq("spider"))
     end
   end
 
