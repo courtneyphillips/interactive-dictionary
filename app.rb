ce5abf05494048d368efd97b02ab2687f2a6eddf
Courtney <courtney.mae.phillips@gmail.com>
2015-04-29 17:57:25 -0700

    Take user input and return terms and their definitions.

--- app.rb
+++ app.rb
@@ -10,9 +10,16 @@ require('./lib/interactive_dictionary')
 get('/entry') do
   term = params.fetch('word')
   definition = params.fetch('definition')
+
+  @definitions = []
   @entry = Word.new(term)
   @definition = Definition.new(definition)
-  
+
+  definition.each() do
+    @definitions.push(@definition)
+  end
+
+  @dictionary_entry = Hash.new({@entry => [@definitions]})
 
   end
     erb(:word_definition)
